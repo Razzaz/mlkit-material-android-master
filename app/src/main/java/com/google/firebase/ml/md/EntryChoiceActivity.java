d38c3087b383a0303f68fc29605f7055837d7d75
Ridho Gani <ridhoadhadigani@gmail.com>
2019-11-30 09:58:03 +0700

    Initial commit

---
+++ app/src/main/java/com/google/firebase/ml/md/EntryChoiceActivity.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.ml.md;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.ml.md.java.LiveBarcodeScanningActivity;
+import com.google.firebase.ml.md.java.MainActivity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EntryChoiceActivity extends AppCompatActivity {
+
+    private static final String TAG = "EntryChoiceActivity";
+    private Button button;
+
+//    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+//    public void addToFirestore() {
+//        Map<String, Object> user = new HashMap<>();
+//        user.put("first", "Ada");
+//        user.put("last", "Lovelace");
+//        user.put("born", 1815);
+//
+//        // Add a new document with a generated ID
+//        db.collection("users")
+//                .add(user)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document", e);
+//                    }
+//                });
+//    }
+    // add this line for git test
+
+//    private enum EntryMode {
+//        ENTRY_JAVA(R.string.entry_java_title, R.string.entry_java_subtitle);
+//        //ENTRY_KOTLIN(R.string.entry_kotlin_title, R.string.entry_kotlin_subtitle);
+//
+//        private final int titleResId;
+//        private final int subtitleResId;
+//
+//        EntryMode(int titleResId, int subtitleResId) {
+//            this.titleResId = titleResId;
+//            this.subtitleResId = subtitleResId;
+//        }
+//    }
+//
+        @Override
+        protected void onCreate(@Nullable Bundle bundle) {
+            super.onCreate(bundle);
+            AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+            setContentView(R.layout.activity_entry_choice);
+
+            button = (Button) findViewById(R.id.buttonGo);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openActivityMain();
+                }
+            });
+
+        }
+
+        public void openActivityMain(){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+//
+//    private class EntryItemAdapter extends RecyclerView.Adapter<EntryItemAdapter.EntryItemViewHolder> {
+//
+//        private final EntryMode[] entryModes;
+//
+//        EntryItemAdapter(EntryMode[] entryModes) {
+//            this.entryModes = entryModes;
+//        }
+//
+//        @NonNull
+//        @Override
+//        public EntryItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//            return new EntryItemViewHolder(
+//                    LayoutInflater.from(parent.getContext())
+//                            .inflate(R.layout.entry_item, parent, false));
+//        }
+//
+//        @Override
+//        public void onBindViewHolder(@NonNull EntryItemViewHolder entryItemViewHolder, int position) {
+//            entryItemViewHolder.bindEntryMode(entryModes[position]);
+//        }
+//
+//        @Override
+//        public int getItemCount() {
+//            return entryModes.length;
+//        }
+//
+//        private class EntryItemViewHolder extends RecyclerView.ViewHolder {
+//
+//            private final TextView titleView;
+//            private final TextView subtitleView;
+//
+//            EntryItemViewHolder(@NonNull View view) {
+//                super(view);
+//                titleView = view.findViewById(R.id.entry_title);
+//                subtitleView = view.findViewById(R.id.entry_subtitle);
+//            }
+//
+//            void bindEntryMode(EntryMode entryMode) {
+//                titleView.setText(entryMode.titleResId);
+//                subtitleView.setText(entryMode.subtitleResId);
+//                itemView.setOnClickListener(view -> {
+//                    Activity activity = EntryChoiceActivity.this;
+//                    //switch (entryMode) {
+//                    //  case ENTRY_JAVA:
+//                        activity.startActivity(new Intent(activity, MainActivity.class));
+//                    //    break;
+//                    //}
+//                });
+//            }
+//        }
+//    }
+}
---
+++ app/src/main/java/com/google/firebase/ml/md/java/LiveBarcodeScanningActivity.java
@@ -0,0 +1,269 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.firebase.ml.md.java;
+
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.chip.Chip;
+import com.google.common.base.Objects;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.ml.md.R;
+import com.google.firebase.ml.md.java.camera.GraphicOverlay;
+import com.google.firebase.ml.md.java.camera.WorkflowModel;
+import com.google.firebase.ml.md.java.camera.WorkflowModel.WorkflowState;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.md.java.barcodedetection.BarcodeField;
+import com.google.firebase.ml.md.java.barcodedetection.BarcodeProcessor;
+import com.google.firebase.ml.md.java.barcodedetection.BarcodeResultFragment;
+import com.google.firebase.ml.md.java.camera.CameraSource;
+import com.google.firebase.ml.md.java.camera.CameraSourcePreview;
+import com.google.firebase.ml.md.java.settings.SettingsActivity;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/** Demonstrates the barcode scanning workflow using camera preview. */
+public class LiveBarcodeScanningActivity extends AppCompatActivity implements OnClickListener {
+
+  private static final String TAG = "LiveBarcodeActivity";
+
+  private FirebaseFirestore db = FirebaseFirestore.getInstance();
+  public void addToFirestore(String value) {
+    Map<String, Object> user = new HashMap<>();
+    user.put("result", value);
+    //user.put("last", "Lovelace");
+    //user.put("born", 1815);
+
+    // Add a new document with a generated ID
+    db.collection("users").document("tapackData")
+            .set(user)
+            .addOnSuccessListener(new OnSuccessListener<Void>() {
+              @Override
+              public void onSuccess(Void aVoid) {
+                Log.d(TAG, "DocumentSnapshot added with ID: ");
+              }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+              @Override
+              public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error adding document", e);
+              }
+            });
+  }
+
+  private CameraSource cameraSource;
+  private CameraSourcePreview preview;
+  private GraphicOverlay graphicOverlay;
+  private View settingsButton;
+  private View flashButton;
+  private Chip promptChip;
+  private AnimatorSet promptChipAnimator;
+  private WorkflowModel workflowModel;
+  private WorkflowState currentWorkflowState;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_live_barcode);
+    preview = findViewById(R.id.camera_preview);
+    graphicOverlay = findViewById(R.id.camera_preview_graphic_overlay);
+    graphicOverlay.setOnClickListener(this);
+    cameraSource = new CameraSource(graphicOverlay);
+
+    promptChip = findViewById(R.id.bottom_prompt_chip);
+    promptChipAnimator =
+        (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.bottom_prompt_chip_enter);
+    promptChipAnimator.setTarget(promptChip);
+
+    findViewById(R.id.close_button).setOnClickListener(this);
+    flashButton = findViewById(R.id.flash_button);
+    flashButton.setOnClickListener(this);
+    settingsButton = findViewById(R.id.settings_button);
+    settingsButton.setOnClickListener(this);
+
+    setUpWorkflowModel();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    workflowModel.markCameraFrozen();
+    settingsButton.setEnabled(true);
+    currentWorkflowState = WorkflowState.NOT_STARTED;
+    cameraSource.setFrameProcessor(new BarcodeProcessor(graphicOverlay, workflowModel));
+    workflowModel.setWorkflowState(WorkflowState.DETECTING);
+  }
+
+  @Override
+  protected void onPostResume() {
+    super.onPostResume();
+    BarcodeResultFragment.dismiss(getSupportFragmentManager());
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    currentWorkflowState = WorkflowState.NOT_STARTED;
+    stopCameraPreview();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    if (cameraSource != null) {
+      cameraSource.release();
+      cameraSource = null;
+    }
+  }
+
+  @Override
+  public void onClick(View view) {
+    int id = view.getId();
+    if (id == R.id.close_button) {
+      onBackPressed();
+
+    } else if (id == R.id.flash_button) {
+      if (flashButton.isSelected()) {
+        flashButton.setSelected(false);
+        cameraSource.updateFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+      } else {
+        flashButton.setSelected(true);
+        cameraSource.updateFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+      }
+
+    } else if (id == R.id.settings_button) {
+      // Sets as disabled to prevent the user from clicking on it too fast.
+      settingsButton.setEnabled(false);
+      startActivity(new Intent(this, SettingsActivity.class));
+    }
+  }
+
+  private void startCameraPreview() {
+    if (!workflowModel.isCameraLive() && cameraSource != null) {
+      try {
+        workflowModel.markCameraLive();
+        preview.start(cameraSource);
+      } catch (IOException e) {
+        Log.e(TAG, "Failed to start camera preview!", e);
+        cameraSource.release();
+        cameraSource = null;
+      }
+    }
+  }
+
+  private void stopCameraPreview() {
+    if (workflowModel.isCameraLive()) {
+      workflowModel.markCameraFrozen();
+      flashButton.setSelected(false);
+      preview.stop();
+    }
+  }
+
+  private void setUpWorkflowModel() {
+    workflowModel = ViewModelProviders.of(this).get(WorkflowModel.class);
+
+    // Observes the workflow state changes, if happens, update the overlay view indicators and
+    // camera preview state.
+    workflowModel.workflowState.observe(
+        this,
+        workflowState -> {
+          if (workflowState == null || Objects.equal(currentWorkflowState, workflowState)) {
+            return;
+          }
+
+          currentWorkflowState = workflowState;
+          Log.d(TAG, "Current workflow state: " + currentWorkflowState.name());
+
+          boolean wasPromptChipGone = (promptChip.getVisibility() == View.GONE);
+
+          switch (workflowState) {
+            case DETECTING:
+              promptChip.setVisibility(View.VISIBLE);
+              promptChip.setText(R.string.prompt_point_at_a_barcode);
+              startCameraPreview();
+              break;
+            case CONFIRMING:
+              promptChip.setVisibility(View.VISIBLE);
+              promptChip.setText(R.string.prompt_move_camera_closer);
+              startCameraPreview();
+              break;
+            case SEARCHING:
+              promptChip.setVisibility(View.VISIBLE);
+              promptChip.setText(R.string.prompt_searching);
+              stopCameraPreview();
+              break;
+            case DETECTED:
+            case SEARCHED:
+              promptChip.setVisibility(View.GONE);
+              stopCameraPreview();
+              break;
+            default:
+              promptChip.setVisibility(View.GONE);
+              break;
+          }
+
+          boolean shouldPlayPromptChipEnteringAnimation =
+              wasPromptChipGone && (promptChip.getVisibility() == View.VISIBLE);
+          if (shouldPlayPromptChipEnteringAnimation && !promptChipAnimator.isRunning()) {
+            promptChipAnimator.start();
+          }
+        });
+
+    workflowModel.detectedBarcode.observe(
+        this,
+        barcode -> {
+          if (barcode != null) {
+            ArrayList<BarcodeField> barcodeFieldList = new ArrayList<>();
+            String rawValue = barcode.getRawValue();
+
+            addToFirestore(barcode.getRawValue());
+            barcodeFieldList.add(new BarcodeField("Raw Value", rawValue));
+            int valueType = barcode.getValueType();
+            switch (valueType) {
+              case FirebaseVisionBarcode.TYPE_WIFI:
+                String ssid = barcode.getWifi().getSsid();
+                String password = barcode.getWifi().getPassword();
+                int type = barcode.getWifi().getEncryptionType();
+                barcodeFieldList.add(new BarcodeField("Wifi Value", ssid));
+                break;
+              case FirebaseVisionBarcode.TYPE_URL:
+                String title = barcode.getUrl().getTitle();
+                String url = barcode.getUrl().getUrl();
+                break;
+            }
+            BarcodeResultFragment.show(getSupportFragmentManager(), barcodeFieldList);
+          }
+        });
+  }
+}
